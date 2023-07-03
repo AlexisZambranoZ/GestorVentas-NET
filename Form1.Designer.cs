edb683b2b61074cb43ec7c6bf1407e6908a767fb
Alexis Zambrano <zambra873@gmail.com>
2023-07-03 10:28:55 -0600

    Terminado

---
+++ Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtBoxProducto = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblPrecioUnitario = new System.Windows.Forms.Label();
+            this.txtBoxPrecio = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.txtboxCantidad = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.lblFecha = new System.Windows.Forms.Label();
+            this.lvVenta = new System.Windows.Forms.ListView();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.txtBoxDesc = new System.Windows.Forms.TextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Milky Honey", 27.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(193, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(408, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "REGISTRO DE VENTAS";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Red;
+            this.btnSalir.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.btnSalir.Location = new System.Drawing.Point(666, 412);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(85, 31);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(581, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Fecha Actual:";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.groupBox1.Controls.Add(this.lblDescripcion);
+            this.groupBox1.Controls.Add(this.txtBoxDesc);
+            this.groupBox1.Controls.Add(this.txtBoxProducto);
+            this.groupBox1.Controls.Add(this.btnRegistrar);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.lblPrecioUnitario);
+            this.groupBox1.Controls.Add(this.btnCancelar);
+            this.groupBox1.Controls.Add(this.txtBoxPrecio);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.lblPrecio);
+            this.groupBox1.Controls.Add(this.lblProducto);
+            this.groupBox1.Controls.Add(this.txtboxCantidad);
+            this.groupBox1.Location = new System.Drawing.Point(12, 101);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(283, 337);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Precio Del Producto:";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // txtBoxProducto
+            // 
+            this.txtBoxProducto.Location = new System.Drawing.Point(9, 47);
+            this.txtBoxProducto.Name = "txtBoxProducto";
+            this.txtBoxProducto.Size = new System.Drawing.Size(261, 20);
+            this.txtBoxProducto.TabIndex = 17;
+            this.txtBoxProducto.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(104, 95);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(19, 20);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "$";
+            // 
+            // lblPrecioUnitario
+            // 
+            this.lblPrecioUnitario.AutoSize = true;
+            this.lblPrecioUnitario.Location = new System.Drawing.Point(6, 77);
+            this.lblPrecioUnitario.Name = "lblPrecioUnitario";
+            this.lblPrecioUnitario.Size = new System.Drawing.Size(76, 13);
+            this.lblPrecioUnitario.TabIndex = 15;
+            this.lblPrecioUnitario.Text = "Precio Unitario";
+            // 
+            // txtBoxPrecio
+            // 
+            this.txtBoxPrecio.Location = new System.Drawing.Point(9, 95);
+            this.txtBoxPrecio.Name = "txtBoxPrecio";
+            this.txtBoxPrecio.Size = new System.Drawing.Size(89, 20);
+            this.txtBoxPrecio.TabIndex = 14;
+            this.txtBoxPrecio.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrecio.Location = new System.Drawing.Point(94, 241);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(102, 31);
+            this.lblPrecio.TabIndex = 13;
+            this.lblPrecio.Text = "lblPrecio";
+            this.lblPrecio.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblPrecio.Click += new System.EventHandler(this.lblPrecio_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(115, 216);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Venta Total";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(199, 77);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Cantidad";
+            // 
+            // lblProducto
+            // 
+            this.lblProducto.AutoSize = true;
+            this.lblProducto.Location = new System.Drawing.Point(6, 29);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(125, 13);
+            this.lblProducto.TabIndex = 10;
+            this.lblProducto.Text = "Seleccion De Productos:";
+            // 
+            // txtboxCantidad
+            // 
+            this.txtboxCantidad.Location = new System.Drawing.Point(170, 93);
+            this.txtboxCantidad.Name = "txtboxCantidad";
+            this.txtboxCantidad.Size = new System.Drawing.Size(100, 20);
+            this.txtboxCantidad.TabIndex = 7;
+            this.txtboxCantidad.TextChanged += new System.EventHandler(this.txtboxCantidad_TextChanged);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btnCancelar.Location = new System.Drawing.Point(9, 293);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 30);
+            this.btnCancelar.TabIndex = 8;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.BackColor = System.Drawing.Color.Orange;
+            this.btnRegistrar.Location = new System.Drawing.Point(202, 293);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 30);
+            this.btnRegistrar.TabIndex = 9;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = false;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // lblFecha
+            // 
+            this.lblFecha.AutoSize = true;
+            this.lblFecha.Location = new System.Drawing.Point(660, 70);
+            this.lblFecha.Name = "lblFecha";
+            this.lblFecha.Size = new System.Drawing.Size(47, 13);
+            this.lblFecha.TabIndex = 3;
+            this.lblFecha.Text = "lblFecha";
+            // 
+            // lvVenta
+            // 
+            this.lvVenta.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvVenta.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.lvVenta.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lvVenta.Cursor = System.Windows.Forms.Cursors.Default;
+            this.lvVenta.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.lvVenta.GridLines = true;
+            this.lvVenta.HideSelection = false;
+            this.lvVenta.Location = new System.Drawing.Point(319, 101);
+            this.lvVenta.Name = "lvVenta";
+            this.lvVenta.Size = new System.Drawing.Size(432, 305);
+            this.lvVenta.TabIndex = 10;
+            this.lvVenta.UseCompatibleStateImageBehavior = false;
+            this.lvVenta.View = System.Windows.Forms.View.Details;
+            this.lvVenta.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // txtBoxDesc
+            // 
+            this.txtBoxDesc.Location = new System.Drawing.Point(9, 172);
+            this.txtBoxDesc.Name = "txtBoxDesc";
+            this.txtBoxDesc.Size = new System.Drawing.Size(261, 20);
+            this.txtBoxDesc.TabIndex = 18;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(115, 146);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(63, 13);
+            this.lblDescripcion.TabIndex = 19;
+            this.lblDescripcion.Text = "Descripcion";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lvVenta);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblFecha);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registro De Ventas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtboxCantidad;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblProducto;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblFecha;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblPrecioUnitario;
+        private System.Windows.Forms.TextBox txtBoxPrecio;
+        private System.Windows.Forms.TextBox txtBoxProducto;
+        private System.Windows.Forms.ListView lvVenta;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox txtBoxDesc;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+
+        double precio = 0;
+
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("¿Estás seguro de que deseas cerrar la aplicación?", "Confirmación", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true; // Cancelar el cierre del formulario
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            this.FormClosing += Form1_FormClosing;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblFecha.Text = DateTime.Today.Date.ToString("d");
+            lblPrecio.Text = (0).ToString("C");
+
+  
+
+            // Agregar las columnas al control ListView
+            lvVenta.Columns.Add("Producto");
+            lvVenta.Columns.Add("Cantidad");
+            lvVenta.Columns.Add("Precio");
+            lvVenta.Columns.Add("Precio Unitario");
+            lvVenta.Columns.Add("Hora");
+            lvVenta.Columns.Add("Descripcion");
+            lvVenta.Columns[5].Width = 200; 
+            // Ajustar el ancho de la quinta columna a 200 píxeles
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lvVenta_SelectedIndexChanged(object sender, EventArgs e)
+        {
+       
+
+        }
+
+        private void txtboxCantidad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            // Variables opcionales con valores predeterminados
+            string producto = string.Empty;
+            double precioProducto = 0;
+            int cantidad = 0;
+            string descripcion = string.Empty;
+
+            // Verificar si se han proporcionado valores en los campos de entrada
+            if (!string.IsNullOrEmpty(txtBoxProducto.Text))
+                producto = txtBoxProducto.Text;
+
+            if (!string.IsNullOrEmpty(txtBoxPrecio.Text))
+                precioProducto = double.Parse(txtBoxPrecio.Text);
+
+            if (!string.IsNullOrEmpty(txtboxCantidad.Text))
+                cantidad = int.Parse(txtboxCantidad.Text);
+
+            if (!string.IsNullOrEmpty(txtBoxDesc.Text))
+                descripcion = txtBoxDesc.Text;
+
+            // Calcular el precio total del producto
+            double precioTotalProducto = precioProducto * cantidad;
+
+            // Actualizar el precio total
+            precio += precioTotalProducto;
+            lblPrecio.Text = precio.ToString("C");
+
+            // Obtener la hora actual
+            DateTime horaActual = DateTime.Now;
+
+            // Agregar los valores a la lista
+            ListViewItem item = new ListViewItem(producto);
+            item.SubItems.Add(cantidad.ToString());
+            item.SubItems.Add(precioTotalProducto.ToString("C"));
+            lvVenta.Items.Add(item);
+            item.SubItems.Add(precioProducto.ToString("C"));
+            item.SubItems.Add(horaActual.ToString("HH:mm:ss")); // Agregar la hora actual
+            item.SubItems.Add(descripcion);
+
+
+            // Limpiar los campos de entrada después de agregar el producto
+            txtBoxProducto.Text = string.Empty;
+            txtBoxPrecio.Text = string.Empty;
+            txtboxCantidad.Text = string.Empty;
+            txtBoxDesc.Text = string.Empty;
+
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+
+            // Limpiar los campos de entrada y restablecer el precio total
+            txtBoxProducto.Text = string.Empty;
+            txtBoxPrecio.Text = string.Empty;
+            txtboxCantidad.Text = string.Empty;
+            precio = 0;
+            lblPrecio.Text = precio.ToString("C");
+
+            // Limpiar la lista
+            lvVenta.Items.Clear();
+
+        }
+
+        private void lblPrecio_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            // Detener el programa y cerrar la aplicación
+            Application.Exit();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+        }
+
+     
+    }
+}
